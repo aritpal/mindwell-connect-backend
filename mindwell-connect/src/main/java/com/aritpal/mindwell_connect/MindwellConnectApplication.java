ace0af49032d6d9a30247a1a9a5e31524a58d15a
aritpal <aritpal1443@gmail.com>
2025-06-11 15:44:33 +0530

    first commit

---
+++ mindwell-connect/src/main/java/com/aritpal/mindwell_connect/MindwellConnectApplication.java
@@ -0,0 +1,13 @@
+package com.aritpal.mindwell_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindwellConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindwellConnectApplication.class, args);
+	}
+
+}
---
+++ mindwell-connect/src/test/java/com/aritpal/mindwell_connect/MindwellConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aritpal.mindwell_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindwellConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
